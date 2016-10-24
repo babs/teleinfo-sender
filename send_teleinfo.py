25907f7dfc0307f2fa3602a95422a531703e3e91
Damien Degois <damien@degois.info>
2016-10-24 21:47:53 +0200

    Add send to UDP

--- send_teleinfo.py
+++ send_teleinfo.py
@@ -3,7 +3,7 @@ import sys, serial, datetime, time, threading, urllib2, json
 from Colorize import colorize
 from Queue import Queue
 
-
+import socket
 
 class glob:
     workthread = None
@@ -12,6 +12,8 @@ class glob:
     counter = 0L
     counterlock = threading.Lock()
     wrap_at = 60
+    socket = None
+    socket_targets = []
 
 def checksum(line):
     return chr((
@@ -57,14 +59,20 @@ def worker():
         glob.trames.task_done()
 
 def main():
+    glob.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    glob.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    glob.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
     if len(sys.argv) > 1:
-        glob.destination = sys.argv[1:]
+        glob.destination = [x for x in sys.argv[1:] if x.startswith('http')]
+        glob.socket_targets = [(ip, int(port)) for ip, port in [x.split(":") for x in sys.argv[1:] if not x.startswith('http')]]
     else:
         print colorize("no destination set!", fg="red", bold=True)
         sys.exit(1)
 
     print colorize("Starting worker", fg="green", bold=True)
-    print colorize("[*] sending to %s"%str(glob.destination), fg="green")
+    print colorize("[*] sending over http to %s." % ", ".join(glob.destination), fg="green")
+    print colorize("[*] sending over udp to %s." % ", ".join(["%s:%d"%(ip, port) for ip, port in glob.socket_targets]), fg="green")
     glob.workthread = threading.Thread(target=worker)
     glob.workthread.daemon = True
     glob.workthread.start()
@@ -77,26 +85,30 @@ def main():
     laststart = None
     while True:
         c = ser.read()
-        if c == "\x02": # Debut de trame
+        if c == "\x02":  # Debut de trame
             tramets = time.time()
             trame = [c]
             duration = ""
             if laststart != None:
-                duration = str(datetime.datetime.now()-laststart)
-            #laststart = datetime.datetime.now()
-            #print "***start %s"%colorize(duration, bold=True)
+                duration = str(datetime.datetime.now() - laststart)
+            # laststart = datetime.datetime.now()
+            # print "***start %s"%colorize(duration, bold=True)
             continue
-        if c == "\x03": # fin de trame
-            #print "***end"
+        if c == "\x03":  # fin de trame
+            # print "***end"
             trame.append(c)
             if tramets != None:
-                glob.trames.put({"tramets": tramets, "trame":  "".join(trame)})
+                structtrame = {"tramets": tramets, "trame":  "".join(trame)}
+                glob.trames.put(structtrame)
+                stringtrame = json.dumps(structtrame)
+                for target in glob.socket_targets:
+                    glob.socket.sendto(stringtrame, target)
                 write_progress(".")
             continue
-        if c == "\x0a": # Debut de ligne
+        if c == "\x0a":  # Debut de ligne
             buf = []
             continue
-        if c == "\x0d": # Fin de ligne
+        if c == "\x0d":  # Fin de ligne
             line = "".join(buf)
             trame.append("\x0a" + line + "\x0d")
         buf.append(c)
